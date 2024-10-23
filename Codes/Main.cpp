afa11816213888b8665b05a917f5680578110672
WhythZ <jichenzong@outlook.com>
2024-10-23 22:21:09 +0800

    10.23 add test case

--- Codes/Main.cpp
+++ Codes/Main.cpp
@@ -5,6 +5,7 @@
 #include "01-LinearList/05-SStack.hpp"
 #include "01-LinearList/06-LQueue.hpp"
 #include "01-LinearList/07-SQueue.hpp"
+
 #include "02-Tree/01-TreeNode.hpp"
 #include "02-Tree/02-TreeIterator.hpp"
 #include "02-Tree/03-BiTreeNode.hpp"
@@ -16,7 +17,15 @@
 #include "02-Tree/09-MultiSearchTree.hpp"
 #include "02-Tree/10-BPlusTree.hpp"
 #include "02-Tree/11-MinHeap.hpp"
+
 #include "03-Sorting/01-SelectionSort.hpp"
+#include "03-Sorting/02-InsertionSort.hpp"
+#include "03-Sorting/03-BubbleSort.hpp"
+#include "03-Sorting/04-HeapSort.hpp"
+#include "03-Sorting/05-MergeSort.hpp"
+#include "03-Sorting/06-QuickSort.hpp"
+#include "03-Sorting/07-BucketSort.hpp"
+#include "03-Sorting/08-RadixSort.hpp"
 
 int main()
 {
@@ -27,6 +36,7 @@ int main()
     // Test_S_Stack::MainTest();
     // Test_L_Queue::MainTest();
     // Test_S_Queue::MainTest();
+
     // Test_Tree_Node::MainTest();
     // Test_Tree_Iterator::MainTest();
     // Test_Bi_Tree_Node::MainTest();
@@ -38,4 +48,13 @@ int main()
     // Test_Multi_Search_Tree::MainTest();
     // Test_B_Plus_Tree::MainTest();
     // Test_Min_Heap::MainTest();
+
+    // Test_Selection_Sort::MainTest();
+    // Test_Insertion_Sort::MainTest();
+    // Test_Bubble_Sort::MainTest();
+    // Test_Heap_Sort::MainTest();
+    // Test_Merge_Sort::MainTest();
+    // Test_Quick_Sort::MainTest();
+    // Test_Bucket_Sort::MainTest();
+    // Test_Radix_Sort::MainTest();
 }
